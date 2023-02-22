0ac08cfdfb1d89dff6d8fe3c28a731d546e9cf9d
Mathew <anjosmathew@gmail.com>
2023-02-22 18:12:47 -0500

    Added godotenv to lookup environment variables. Moved host & port to environment variables. Added .env to gitignore

--- internal/server/main.go
+++ internal/server/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/MarioScripts/vlr-api/proto/gen/vlr/v1"
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 )
-
-var addr string = "0.0.0.0:50051"
 
 type Server struct {
 	pb.VlrServer
 }
 
 func main() {
+	err2 := godotenv.Load()
+	if err2 != nil {
+		log.Fatalf("Error loading .env file %v\n", err2)
+	}
+
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf("%s:%s", host, port)
+
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("Failed to listen of: %v\n", err)
